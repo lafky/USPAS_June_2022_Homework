b406278e2748f5c3e8b8a9b77233d7bdb6d8863e
Mike Lafky <mike.lafky@gmail.com>
2022-06-08 23:23:27 +1000

    better plotting, fft questionable

--- 13.py
+++ 13.py
@@ -18,22 +18,33 @@ eField = np.zeros(len(time),dtype=complex)    #Dependent Axis, Calculated E Fiel
 timej = (np.random.rand(numE)*bunchLength)-bunchLength*.5 #Generate a random distribution of times for temporal incoherence, corrected +/- half the bunch length
 #timej = np.zeros(numE)
 #np.round(timej,2)              #Doesn't work, don't know why
-sigmaT = 2
+sigmaT = 5
+modes = bunchLength/(sigmaT*4)
 omegaOne = 2*pi
 
 for t in range(len(time)):  #Calculates the E field at each unit of time
     for y in range(numE):
         eField[t] = eField[t] + cmath.exp( (-((time[t]-timej[y])**2)/(4*(sigmaT**2))) - 1j*omegaOne*(time[t]-timej[y]) )
     #print(t, y, time[t], eField[t])
+
 #Calculate the Fourier Transform
-efft = fft(eField)[:len(eField)//2]
-tf = fftfreq(len(eField),150)[:len(eField)//2]
+#efft = np.fft.fft(eField)[:len(eField)//2]
+efft = np.fft.fft(eField)
+efft = np.abs(efft)
+#tf = fftfreq(len(eField),150)[:len(eField)//2]
+tf = fftfreq(len(eField))
+
+#tnum = len(eField)
+#tnum2 = int(tnum/2)
+#freq = time[:tnum2]/(len(time)*.1)
 
 fig1, axs = plt.subplots(1,2,figsize=(16,9))
 fig1.suptitle('')
 axs[0].plot(time,eField)
 axs[0].set_title('E(t) vs t')
+axs[0].text(3,4,'sigmaT = ' + str(sigmaT) + ' # modes = ' + str(int(modes)))
 axs[1].plot(tf,efft)
+#axs[1].plot(freq,efft)
 axs[1].set_title('E(t) vs s')
 #plt.plot(time,eField)
 #plt.plot(tf, efft)
