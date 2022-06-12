020ac0ecb783db4aee44fd886754ed63dfe7fdcc
Mike Lafky <mike.lafky@gmail.com>
2022-06-13 04:55:26 +1000

    fixed a comment

--- HW24a.py
+++ HW24a.py
@@ -23,7 +23,7 @@ gap = .015                  #gap, m
 
 #Calculated undulator parameters
 B0 = 3.44*math.exp(-(gap/lam_u)*(5.08-1.54*(gap/lam_u)))    #Field strength, T
-k = 93.4*lam_u*B0                                           #deflection parameter, m*T
+k = 93.4*lam_u*B0                                           #deflection parameter, unitless
 
 #Calculated fundamental parameters
 lam_1 = (lam_u/(2*gamma*gamma))*(1+k*k/2)                   #fundamental wave length, m
