9c3c780be98aa835612e8fb7735a68088a4a5d80
Mike Lafky <mike.lafky@gmail.com>
2022-06-30 12:10:24 +1000

    added BW

--- HW61.py
+++ HW61.py
@@ -35,12 +35,15 @@ B = 2*rho*ku*z*mu*compl     #Exponent
 #print(mu, compl, B)
 #print(6.234E-4*ku*70)
 
-k = np.logspace(.1,10,100)  #Array of K values
+k = np.logspace(.1,10,1000)  #Array of K values
 h = np.array([ 3, 5, 7, 9])  #Array of harmonics
 p_h = np.zeros((len(h),len(k)))      #Power calc array
+rho = np.zeros((len(h),len(k)))      #bandwidth array
 
 #print(p_h)
 #print(k)
+
+#Now I have to ratio of the harmonic rho to the fundamental at each harmonic
 
 for x in range(len(h)):
     for y in range(len(k)):
@@ -51,20 +54,31 @@ for x in range(len(h)):
         x_h = (h[x]*(k[y]**2))/(4+2*(k[y]**2))                                           #input for harmonic bessell function function
         jj_h = ((-1)**h[x])*(special.jv((h[x]-1)/2,(x_h))-special.jv((h[x]+1)/2,(x_h)))  #bessell function function at each harmonic
 
+        rho_p_h = (((k[y]*jj_h)/(1+(k[y]**2)/2))**(2))  #pierce parameter at the harmonic
+        rho_f_h = (((k[y]*jj_1)/(1+(k[y]**2)/2))**(2))  #pierce parameter at the fundamental
+        rho[x][y] = (rho_p_h/rho_f_h)**(1/3)
+
         p_h[x][y] = ((h[x]*(jj_h**2)/(jj_1)**2))**(1/3)
 
 #print(jj_1**2,jj_h[0]**2, p_h)
 #Plot
-
+print(rho)
 fig, ax1 = plt.subplots()
-ax1.plot(k, p_h[0], label = '3rd')
-ax1.plot(k, p_h[1], label = '5th')
-ax1.plot(k, p_h[2], label = '7th')
-ax1.plot(k, p_h[3], label = '9th')
+ax2 = ax1.twinx()
+ax1.plot(k, p_h[0], 'b', label = '3rd')
+ax2.plot(k, rho[0], 'bx', label = '3rd BW')
+ax1.plot(k, p_h[1], 'r', label = '5th')
+ax2.plot(k, rho[1], 'rx', label = '5th BW')
+ax1.plot(k, p_h[2], 'y', label = '7th')
+ax2.plot(k, rho[2], 'yx', label = '7th BW')
+ax1.plot(k, p_h[3], 'g', label = '9th')
+ax2.plot(k, rho[3], 'gx', label = '9th BW')
 ax1.legend()
+ax2.legend(loc=1)
 ax1.set_yscale('linear')
 ax1.set_xscale('linear')
 ax1.set_xlim([0,10])
 ax1.set_xlabel('k')
 ax1.set_ylabel('mu_h')
+ax2.set_ylabel('BW (% of Fundamental BW)')
 plt.show()
